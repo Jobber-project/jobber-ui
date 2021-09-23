d564673ce6c703b605440d1bfc97e9e5abfa1fe3
Jesper Johansson <johansson.jesper@outlook.com>
2021-09-23 20:33:47 +0200

    feat: update .storybook/main.js with stories paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,4 @@
 module.exports = {
-  addons: [
-    '@storybook/addon-essentials',
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials'],
 }
