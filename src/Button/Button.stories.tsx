eca7c2430714b70dfbe5bec3eb311c389aaa81ca
Jesper Johansson <johansson.jesper@outlook.com>
2021-09-23 21:39:52 +0200

    feat(button): update example button

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,20 +1,40 @@
 import React from 'react'
+import { ComponentMeta } from '@storybook/react'
 
 import Button from './Button'
 
-export function AllButtons() {
-  return (
-    <>
-      <Button>Button</Button>
-    </>
-  )
+export function Intro(props) {
+  return <Button {...props} />
 }
 
-AllButtons.storyName = 'All'
+Intro.storyName = 'Intro'
 
-const story = {
+export function Primary() {
+  return <Button variant="primary">Primary</Button>
+}
+
+Primary.storyName = 'Primary'
+
+export function Secondary() {
+  return <Button variant="secondary">Secondary</Button>
+}
+
+Secondary.storyName = 'Secondary'
+
+const story: ComponentMeta<typeof Button> = {
   title: 'Design System/Button',
   component: Button,
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary'],
+      defaultValue: 'primary',
+    },
+    onClick: { action: 'onClick' },
+    children: {
+      control: { type: 'text' },
+      defaultValue: 'Button',
+    },
+  },
 }
 
 export default story
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,14 +1,55 @@
-import React from 'react'
+import React, { FC } from 'react'
 import styled from 'styled-components'
 
-const Container = styled.button``
+const Container = styled.button<{ $variant?: 'primary' | 'secondary' }>`
+  appearance: none;
+  border-width: 1px;
+
+  ${props =>
+    props.$variant === 'primary' &&
+    `
+    border-color: black;
+    color: blue;
+  `}
+
+  ${props =>
+    props.$variant === 'secondary' &&
+    `
+    border-color: gray;
+    color: green;
+  `}
+`
 
 type ButtonProps = {
-  children?: React.ReactNode
+  /**
+   * Appearance variant
+   */
+  variant?: 'primary' | 'secondary'
+  /**
+   * Button element type attribute
+   */
+  type?: 'button' | 'submit'
+  /**
+   * Regular onClick handler
+   */
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  /**
+   * Button content
+   */
+  children?: string
 }
 
-function Button({ children }: ButtonProps) {
-  return <Container>{children}</Container>
+const Button: FC<ButtonProps> = ({
+  variant = 'primary',
+  type = 'button',
+  onClick,
+  children,
+}) => {
+  return (
+    <Container $variant={variant} type={type} onClick={onClick}>
+      {children}
+    </Container>
+  )
 }
 
 export default Button
