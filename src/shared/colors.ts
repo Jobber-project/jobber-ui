fa8a78417463e2e71d0e7f0e7f962e8472e430d9
Jesper Johansson <johansson.jesper@outlook.com>
2021-11-04 17:35:04 +0100

    refactor: move colorUtils.js to shared/colors.ts

---
+++ src/shared/colors.ts
@@ -0,0 +1,19 @@
+// https://chir.ag/projects/name-that-color/#6B53FF
+const COLORS = {
+  havelockBlue: '#5971dd',
+  electricViolet: '#8d49f7',
+  cornflowerBlue: '#6b53ff',
+  mischa: '#dddfe5',
+  emerald: '#41c879',
+  yellowOrange: '#fda146',
+  carnation: '#fb5255',
+  supernova: '#ffc600',
+  sunshade: '#ff9e2c',
+  persianIndigo: '#371789',
+  white: '#ffffff',
+  black: '#000000',
+}
+
+export const PRIMARY_GRADIENT = `linear-gradient(90deg, ${COLORS.sunshade} 0%, ${COLORS.supernova} 100%)`
+
+export default COLORS
