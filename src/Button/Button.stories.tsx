b1db0f26c408060865eb2b01ff9a22ccd4661a23
Willie Björnbom <willie.sb@live.se>
2021-11-04 17:31:42 +0100

    feat: add button component

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -9,17 +9,81 @@ export function Intro(props) {
 
 Intro.storyName = 'Intro'
 
+export function Default() {
+  return (
+    <>
+      <Button variant="default">Default</Button>
+      <Button outlined variant="default">
+        Outlined
+      </Button>
+    </>
+  )
+}
+
 export function Primary() {
-  return <Button variant="primary">Primary</Button>
+  return (
+    <>
+      <Button variant="primary">Primary</Button>
+      <Button outlined variant="primary">
+        Outlined
+      </Button>
+    </>
+  )
 }
 
 Primary.storyName = 'Primary'
 
 export function Secondary() {
-  return <Button variant="secondary">Secondary</Button>
+  return (
+    <>
+      <Button variant="secondary">Secondary</Button>
+      <Button outlined variant="secondary">
+        Outlined
+      </Button>
+    </>
+  )
 }
 
 Secondary.storyName = 'Secondary'
+
+export function Success() {
+  return (
+    <>
+      <Button variant="success">Success</Button>
+      <Button outlined variant="success">
+        Outlined
+      </Button>
+    </>
+  )
+}
+
+Success.storyName = 'Success'
+
+export function Warning() {
+  return (
+    <>
+      <Button variant="warning">Warning</Button>
+      <Button outlined variant="warning">
+        Outlined
+      </Button>
+    </>
+  )
+}
+
+Warning.storyName = 'Warning'
+
+export function Error() {
+  return (
+    <>
+      <Button variant="error">Error</Button>
+      <Button outlined variant="error">
+        Outlined
+      </Button>
+    </>
+  )
+}
+
+Error.storyName = 'Error'
 
 const story: ComponentMeta<typeof Button> = {
   title: 'Design System/Button',
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,30 +1,110 @@
 import React, { FC } from 'react'
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 
-const Container = styled.button<{ $variant?: 'primary' | 'secondary' }>`
+import { COLORS, PRIMARY_GRADIENT } from '../utils/colorUtils'
+
+const primaryTheme = {
+  border: COLORS.electricViolet,
+  background: `linear-gradient(90deg, ${COLORS.electricViolet} 0%, ${COLORS.havelockBlue} 100%)`,
+  text: COLORS.white,
+}
+
+function getTheme(variant) {
+  switch (variant) {
+    case 'primary':
+      return {
+        border: COLORS.electricViolet,
+        background: `linear-gradient(90deg, ${COLORS.electricViolet} 0%, ${COLORS.havelockBlue} 100%)`,
+        text: COLORS.white,
+      }
+    case 'secondary':
+      return {
+        border: COLORS.sunshade,
+        background: PRIMARY_GRADIENT,
+        text: COLORS.persianIndigo,
+      }
+    case 'success':
+      return {
+        border: COLORS.emerald,
+        background: COLORS.emerald,
+        text: COLORS.white,
+      }
+    case 'warning':
+      return {
+        border: COLORS.yellowOrange,
+        background: COLORS.yellowOrange,
+        text: COLORS.black,
+      }
+    case 'error':
+      return {
+        border: COLORS.carnation,
+        background: COLORS.carnation,
+        text: COLORS.white,
+      }
+    default:
+      return {
+        border: COLORS.havelockBlue,
+        background: COLORS.white,
+        text: COLORS.black,
+      }
+  }
+}
+
+function getOutlined({ $outlined }): string {
+  if (!$outlined) return ''
+  return ``
+}
+
+const Container = styled.button<{
+  $variant?:
+    | 'default'
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'warning'
+    | 'error'
+  $size?: 'small' | 'medium' | 'large'
+  $outlined: boolean
+}>`
+  cursor: pointer;
   appearance: none;
-  border-width: 1px;
+  border-radius: 10px;
+  height: 40px;
+  padding: 8px 16px;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 0.7619047619em; // 16/21
+  border: none;
+  background: ${props => props.theme.background};
+  color: ${props => props.theme.text};
+
+  ${getOutlined}
 
   ${props =>
-    props.$variant === 'primary' &&
+    props.$outlined &&
     `
-    border-color: black;
-    color: blue;
-  `}
+    border: 1px solid ${props.theme.border};
+    background: white;
+    color: ${props.theme.border};
 
-  ${props =>
-    props.$variant === 'secondary' &&
-    `
-    border-color: gray;
-    color: green;
-  `}
+    &:hover {
+      border-width: 2px
+    }
+  `};
 `
 
 type ButtonProps = {
   /**
    * Appearance variant
    */
-  variant?: 'primary' | 'secondary'
+  variant?:
+    | 'default'
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'warning'
+    | 'error'
   /**
    * Button element type attribute
    */
@@ -34,21 +114,46 @@ type ButtonProps = {
    */
   onClick?: React.MouseEventHandler<HTMLButtonElement>
   /**
+   * Button size
+   */
+  size?: 'small' | 'medium' | 'large'
+  /**
+   * Button outlined
+   */
+  outlined?: boolean
+  /**
+   * Button disabled
+   */
+  disabled?: boolean
+  /**
    * Button content
    */
   children?: string
 }
 
 const Button: FC<ButtonProps> = ({
-  variant = 'primary',
+  variant = 'default',
   type = 'button',
   onClick,
+  size = 'medium',
+  outlined = false,
+  disabled = false,
   children,
 }) => {
   return (
-    <Container $variant={variant} type={type} onClick={onClick}>
-      {children}
-    </Container>
+    <ThemeProvider theme={() => getTheme(variant)}>
+      <Container
+        $variant={variant}
+        type={type}
+        onClick={onClick}
+        $size={size}
+        $outlined={outlined}
+        disabled={disabled}
+        height="false"
+      >
+        {children}
+      </Container>
+    </ThemeProvider>
   )
 }
 
