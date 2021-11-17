15e11a39c5cf8a384fc844fe2f4fb6739d1367d5
Jesper Johansson <johansson.jesper@outlook.com>
2021-11-17 17:31:41 +0100

    fix(TextField): padding and height not correct with design

--- src/TextField/TextField.tsx
+++ src/TextField/TextField.tsx
@@ -149,7 +149,7 @@ function getPadding({ $size }: { $size: TextFieldSize }): string {
 
     case 'medium':
     default:
-      return '10px 16px'
+      return '8px 16px'
   }
 }
 
@@ -201,6 +201,17 @@ function getPaddingLeft({
   }
 }
 
+function getInputHeight({ $size }: { $size: TextFieldSize }): number {
+  switch ($size) {
+    case 'large':
+      return 53
+
+    case 'medium':
+    default:
+      return 40
+  }
+}
+
 function getInputFontSize({ $size }: { $size: TextFieldSize }): number {
   switch ($size) {
     case 'large':
@@ -215,11 +226,11 @@ function getInputFontSize({ $size }: { $size: TextFieldSize }): number {
 function getInputLineHeight({ $size }: { $size: TextFieldSize }): number {
   switch ($size) {
     case 'large':
-      return 1.171667 // 21.09px
+      return 1.1667 // 21px
 
     case 'medium':
     default:
-      return 1.171875 // 18.75px
+      return 1.1875 // 19px
   }
 }
 
@@ -265,7 +276,9 @@ const Input = styled.input<{
   $size: TextFieldSize
   $iconAlign: TextFieldIconAlign
 }>`
-  display: block;
+  display: flex;
+  align-items: center;
+  height: ${getInputHeight}px;
   margin: 0;
   padding: ${getPadding};
   box-sizing: border-box;
