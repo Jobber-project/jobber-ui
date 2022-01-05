20633bd242a0e3fdf5b7bbdeb2dd82bb68bb3ca3
Jesper Johansson <johansson.jesper@outlook.com>
2022-01-05 18:29:58 +0100

    fix: export types explicitly

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import GlobalStyle, { globalStyle } from './GlobalStyle/GlobalStyle'
-import Button from './Button/Button'
-import Checkbox from './Checkbox/Checkbox'
+import Button, { ButtonSize, ButtonVariant } from './Button/Button'
+import Checkbox, { CheckboxVariant } from './Checkbox/Checkbox'
 import RadioButton from './RadioButton/RadioButton'
-import TextField from './TextField/TextField'
+import TextField, {
+  TextFieldIconAlign,
+  TextFieldSize,
+  TextFieldVariant,
+} from './TextField/TextField'
 
-export * from './Button/Button'
-export * from './Checkbox/Checkbox'
-export * from './RadioButton/RadioButton'
-export * from './TextField/TextField'
-
-export { GlobalStyle, globalStyle, Button, Checkbox, RadioButton, TextField }
+export {
+  GlobalStyle,
+  globalStyle,
+  Button,
+  Checkbox,
+  RadioButton,
+  TextField,
+  /* Types */
+  ButtonSize,
+  ButtonVariant,
+  CheckboxVariant,
+  TextFieldIconAlign,
+  TextFieldSize,
+  TextFieldVariant,
+}
