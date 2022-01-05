14b17519c08ca28a690f812ba6ded712bacf1a69
Jesper Johansson <johansson.jesper@outlook.com>
2022-01-05 19:02:37 +0100

    fix: module exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import GlobalStyle, { globalStyle } from './GlobalStyle/GlobalStyle'
-import Button from './Button/Button'
-import Checkbox from './Checkbox/Checkbox'
+import Button, { ButtonSize, ButtonVariant } from './Button/Button'
+import Checkbox, { CheckboxVariant } from './Checkbox/Checkbox'
 import RadioButton from './RadioButton/RadioButton'
-import TextField from './TextField/TextField'
+import TextField, {
+  TextFieldIconAlign,
+  TextFieldSize,
+  TextFieldVariant,
+} from './TextField/TextField'
+
+export type {
+  ButtonSize,
+  ButtonVariant,
+  CheckboxVariant,
+  TextFieldIconAlign,
+  TextFieldSize,
+  TextFieldVariant,
+}
 
 export { GlobalStyle, globalStyle, Button, Checkbox, RadioButton, TextField }
