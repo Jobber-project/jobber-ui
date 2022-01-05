85390a009f4199610547d502a4b8a1ea208fb94f
Jesper Johansson <johansson.jesper@outlook.com>
2022-01-05 17:43:06 +0100

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,9 @@ import Checkbox from './Checkbox/Checkbox'
 import RadioButton from './RadioButton/RadioButton'
 import TextField from './TextField/TextField'
 
+export * from './Button/Button'
+export * from './Checkbox/Checkbox'
+export * from './RadioButton/RadioButton'
+export * from './TextField/TextField'
+
 export { GlobalStyle, globalStyle, Button, Checkbox, RadioButton, TextField }
